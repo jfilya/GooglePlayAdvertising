5136f72a70bcbd3a828aab30bc20e3e980dfd6aa
Julia Filippova <julia.94@bk.ru>
2022-07-19 23:40:15 +0300

    fix: corret change language

--- js/switchLanguage.js
+++ js/switchLanguage.js
@@ -5,16 +5,20 @@ const changeURLLanguage = () => {
 };
 changeURLLanguage();
 const changeLanguage = () => {
-  let hash = window.location.hash;
-  hash = hash.substr(-2);
-  console.log(hash);
+  let hash = window.location.hash.substr(-2);
   if (!allLanguage.includes(hash)) {
     location.href = window.location.pathname + "#?lng=en";
   }
-for(let key in LangArray) {
-    if(document.querySelector("."+key)){
-       document.querySelector("."+key).innerHTML = LangArray[key][hash]; 
+  for (let key in LangArray) {
+    if (document.querySelector("." + key)) {
+      document.querySelector("." + key).innerHTML = LangArray[key][hash];
     }
-}
+  }
 };
 changeLanguage();
+
+window.onhashchange = function () {
+  lang = window.location.hash.substr(-2);
+  location.href = window.location.pathname + "#?lng=" + lang;
+  changeLanguage();
+};
